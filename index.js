85d0df90f7cd70f1b0ceb992267b1ef86345cac1
Marc Joseph <marc@lvlupstudios.com>
2020-06-07 15:35:54 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
+const app = express();
+
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
